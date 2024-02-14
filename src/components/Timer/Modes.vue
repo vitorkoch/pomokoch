cf0f1aca7fb051ca3a333339c011cbcaf29223be
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-02-14 14:40:07 -0300

    style: fix mode width

--- src/components/Timer/Modes.vue
+++ src/components/Timer/Modes.vue
@@ -13,7 +13,7 @@ const timerData = useStore(timerStore);
     <button
       type="button"
       :class="[
-        'w-30 flex items-center justify-center rounded-l-2xl p-2 text-center transition-[background-color,width] duration-300 hover:bg-accent/10 active:bg-dark/20',
+        'flex w-32 items-center justify-center rounded-l-2xl p-2 text-center transition-[background-color,width] duration-300 hover:bg-accent/10 active:bg-dark/20',
         [timerData.mode === 'pomodoro' ? activeStyles : ''],
       ]"
       @click="changeMode('pomodoro')"
@@ -23,7 +23,7 @@ const timerData = useStore(timerStore);
     <button
       type="button"
       :class="[
-        'w-30 flex items-center justify-center border-x-2 border-light/20 p-2 text-center transition-[background-color] duration-300 hover:bg-accent/10 active:bg-dark/20',
+        'flex w-32 items-center justify-center border-x-2 border-light/20 p-2 text-center transition-[background-color] duration-300 hover:bg-accent/10 active:bg-dark/20',
         [timerData.mode === 'shortbreak' ? activeStyles : ''],
       ]"
       @click="changeMode('shortbreak')"
@@ -33,7 +33,7 @@ const timerData = useStore(timerStore);
     <button
       type="button"
       :class="[
-        'w-30 flex items-center justify-center rounded-r-2xl p-2 text-center transition-[background-color] duration-300 hover:bg-accent/10 active:bg-dark/20',
+        'flex w-32 items-center justify-center rounded-r-2xl p-2 text-center transition-[background-color] duration-300 hover:bg-accent/10 active:bg-dark/20',
         [timerData.mode === 'longbreak' ? activeStyles : ''],
       ]"
       @click="changeMode('longbreak')"

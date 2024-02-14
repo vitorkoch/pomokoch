7bb63de897838187cc5879ca70339514215738a9
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-02-14 14:51:50 -0300

    feat: add a footer

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Heart } from "lucide-vue-next";
+</script>
+<template>
+  <footer
+    class="flex flex-col items-center justify-center gap-2 rounded-2xl border-2 border-accent/40 bg-primary p-4"
+  >
+    <span>
+      Hi! I'm <strong class="text-accent">Vitor Koch</strong>, discover more
+      about me
+      <a href="https://media.vitorkoch.com" class="text-accent underline">
+        here
+      </a>
+    </span>
+    <span>Have a good productivity! <Heart class="inline text-accent" /></span>
+  </footer>
+</template>

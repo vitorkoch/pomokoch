c7d6441ca97aff5cfd73f778b28a69ce1a89a390
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-02-15 15:10:41 -0300

    feat: start timer on mount

--- src/components/Timer/Root.vue
+++ src/components/Timer/Root.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { timerStore, restart as restartTimer } from "../../stores/timerStore";
+import { useStore } from "@nanostores/vue";
+
+const timerData = useStore(timerStore);
+
+onMounted(() => {
+  restartTimer();
+
+  setInterval(() => {
+    const { timer, mode } = timerData.value;
+    if (!timer || !mode) return;
+    timerStore.setKey("progressPercentage", timer.percentageOf(mode));
+    timerStore.setKey("display", timer.getDisplay(mode));
+  }, 100);
+});
+</script>
+
 <template>
   <div
     class="my-2 flex flex-col items-center justify-center gap-4 rounded-2xl border-2 border-accent/40 bg-primary p-4 md:p-12"

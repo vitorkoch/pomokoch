e5dc24add1afa9a31db9fe051df30b8a5158b4f3
Vitor Koch <vitorgabrielkoch@gmail.com>
2024-02-17 16:55:48 -0300

    feat: add error label component

---
+++ src/components/ErrorLabel/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  errorMessage: string;
+}>();
+import { XCircle } from "lucide-vue-next";
+</script>
+
+<template>
+  <span
+    class="flex flex-row items-center justify-center gap-1 p-1 text-center font-semibold text-red-500"
+  >
+    <XCircle />
+    {{ errorMessage }}
+  </span>
+</template>
